58cd53792f4856f951b195ddd19e2ff17fbd01aa
Cassiana Caires <100693203+Cassiana13@users.noreply.github.com>
2024-01-19 22:40:36 -0300

    Update main.js

--- Desafio Stage04/main.js
+++ Desafio Stage04/main.js
@@ -5,60 +5,76 @@ alert("Hello World");
 /*
 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
 */
-Number1 = 35;
-Number2 = 31;
+const NumberOne = 35;
+const Numbertwo = 31;
 
-alert(Number1 + Number2);
+alert(NumberOne + NumberTwo);
 /*
 3. Crie um script que declare uma variável e verifique se o seu valor é um número.
  Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
 */
-isa = 5;
+const oneNumber = 20
 
-resultado =
-  isa == Number ? "A variável é um número" : "Sua variável não é um número";
+if (typeof isNumber == 'number') {
+  alert('É um número')
+} else {
+  alert('Não é um número')
+}
+//4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
 
-alert(resultado);
+const isString = 'Isso é uma string'
+
+if (typeof isString == 'string') {
+  alert('É uma string')
+} else {
+  alert('Não é uma string')
+}
+
 /*
 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
 */
-friend = "olá";
-friend == false ? "É um booleano" : "Não é um booleano";
-/*
-6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
-*/
-number1 = 25;
-number2 = 26;
+const isBoolean = true
 
-alert(number1 - number2);
-/*
-7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
-*/
-number1 = 25;
-number2 = 2;
+if (typeof isBoolean == 'boolean') {
+  alert('É um booleano')
+} else {
+  alert('Não é um booleano')
+}
 
-alert(number1 * number2);
-/*
-8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
-*/
-number1 = 25;
-number2 = 32;
-alert(number1 / number2);
-/*
-9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
-*/
-friday = 10;
-if (friday % 2 == 0) {
+//6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+const numberOne = 25;
+const numberTwo = 26;
+
+alert(numberOne - numberTwo);
+
+//7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+const numberOne = 25;
+const numbertwo = 2;
+
+alert(numberOne * numberTwo);
+
+//8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+const numberOne = 25;
+const numberTwo = 32;
+alert(numberOne / numberTwo);
+
+//9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+const eventNumber = 10;
+if (eventNumber % 2 == 0) {
   alert("É um número Par");
 } else {
   alert("Não é um número Par");
 }
-/*
-10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
-*/
 
-numberOne = 6;
-if (numberOne % 2 !== 0) {
+//10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+
+const oddNumber = 6;
+if (oddNumber % 2 !== 0) {
   alert("É um número Ímpar");
 } else {
   alert("Não é um número Ímpar");
