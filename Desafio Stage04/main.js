36e37ca94ad61f0f711aba2d107493c1a1e08791
Cassiana Caires <100693203+Cassiana13@users.noreply.github.com>
2024-01-19 22:30:40 -0300

    Add files via upload

---
+++ Desafio Stage04/main.js
@@ -0,0 +1,65 @@
+/*
+1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+*/
+alert("Hello World");
+/*
+2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+*/
+Number1 = 35;
+Number2 = 31;
+
+alert(Number1 + Number2);
+/*
+3. Crie um script que declare uma variável e verifique se o seu valor é um número.
+ Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+*/
+isa = 5;
+
+resultado =
+  isa == Number ? "A variável é um número" : "Sua variável não é um número";
+
+alert(resultado);
+/*
+5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+*/
+friend = "olá";
+friend == false ? "É um booleano" : "Não é um booleano";
+/*
+6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+*/
+number1 = 25;
+number2 = 26;
+
+alert(number1 - number2);
+/*
+7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+*/
+number1 = 25;
+number2 = 2;
+
+alert(number1 * number2);
+/*
+8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+*/
+number1 = 25;
+number2 = 32;
+alert(number1 / number2);
+/*
+9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+*/
+friday = 10;
+if (friday % 2 == 0) {
+  alert("É um número Par");
+} else {
+  alert("Não é um número Par");
+}
+/*
+10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+*/
+
+numberOne = 6;
+if (numberOne % 2 !== 0) {
+  alert("É um número Ímpar");
+} else {
+  alert("Não é um número Ímpar");
+}
